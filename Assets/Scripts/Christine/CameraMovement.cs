694edeaca641178172c7a61e16e767177ff851e7
Christine <scarletlefairy@yahoo.de>
2016-12-17 22:38:22 +0100

    Working on the ball.

--- Assets/Scripts/Christine/CameraMovement.cs
+++ Assets/Scripts/Christine/CameraMovement.cs
@@ -19,7 +19,7 @@ public class CameraMovement : MonoBehaviour {
             Vector2 shakePos = Random.insideUnitCircle * shakeAmount;
             transform.position = new Vector3(transform.position.x + shakePos.x, transform.position.y + shakePos.y, transform.position.z);
             shakeTimer -= Time.deltaTime;
-
+            
             // TODO include Controller vibrations :3
         }
 
