438fce219ac2706bc2f5138da9fe2e26d98fd49a
Ravart <me@ravart.cc>
2016-12-17 12:47:12 +0100

    Ball added

---
+++ Assets/Scripts/Damiano/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Player owner = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void Launch(){
+
+	}
+
+	/*bool IsGrounded() {
+		return Physics.Raycast(transform.position, -Vector3.up, groundlevel + 0.1f);
+	}*/
+
+
+}
