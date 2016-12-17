81d75075b8aa59d66efd07d5fab7c748746287e6
Lisa Gonaus <lisa.gonaus@students.fh-hagenberg.at>
2016-12-17 15:21:20 +0100

    Added Sound Settings Menu

---
+++ Assets/Scripts/Lisa/NavigateToMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigateToMenu : MonoBehaviour
+{
+    public GameObject MenuToHide, MenuToShow;
+
+    public void SwitchMenu()
+    {
+        MenuToHide.gameObject.SetActive(false);
+        MenuToShow.gameObject.SetActive(true);
+    }
+}
