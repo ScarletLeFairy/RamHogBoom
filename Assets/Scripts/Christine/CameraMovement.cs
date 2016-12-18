99f0cee998ba53dc4b06bfd5a5366fd76eaf3c43
Christine <scarletlefairy@yahoo.de>
2016-12-18 20:48:00 +0100

    adapted camera script, now not shaking on Button A anymore.

--- Assets/Scripts/Christine/CameraMovement.cs
+++ Assets/Scripts/Christine/CameraMovement.cs
@@ -6,35 +6,19 @@ public class CameraMovement : MonoBehaviour {
     private float shakeAmount;
     private float shakeTimer;
 
-    private GameObject currentTarget;
-    public GameObject targetMiddle; // assign in editor
-    public GameObject targetMiddleLeft; // assign in editor
-    public GameObject targetMiddleRight; // assign in editor
-    public GameObject targetLeft; // assign in editor
-    public GameObject targetRight; // assign in editor
-
-    void Start ()
-    {
-        // set middle plane as start target
-        currentTarget = targetMiddle;            
-    }
-    
-
     // Update is called once per frame
     void Update () {
 
-        if (GetButtonA())
-        {
-            ShakeCamera(0.1f, 1);
-        }
+        //if (GetButtonA())
+        //{
+        //    ShakeCamera(0.1f, 1);
+        //}
 
         if (shakeTimer >= 0)
         {
             Vector2 shakePos = Random.insideUnitCircle * shakeAmount;
             transform.position = new Vector3(transform.position.x + shakePos.x, transform.position.y + shakePos.y, transform.position.z);
-            shakeTimer -= Time.deltaTime;
-            
-            // TODO include Controller vibrations :3
+            shakeTimer -= Time.deltaTime;           
         }
 
     }
@@ -45,38 +29,9 @@ public class CameraMovement : MonoBehaviour {
         shakeTimer = shakeDuration;
     }
 
-
-    private bool GetButtonA()
-    {
-        return Input.GetKeyDown(KeyCode.JoystickButton0);
-    }
-
-
-
-    public void SwitchToMiddlePlane()
-    {
-
-    }
-
-    public void SwitchToMiddleRightPlane()
-    {
-
-    }
-
-    public void SwitchToRightPlane()
-    {
-
-    }
-
-    public void SwitchToMiddleLeftPlane()
-    {
-
-    }
-
-    public void SwitchToLeftPlane()
-    {
-
-    }
-
+    //private bool GetButtonA()
+    //{
+    //    return Input.GetKeyDown(KeyCode.JoystickButton0);
+    //}
 
 }
