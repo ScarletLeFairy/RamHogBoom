82569737c026a363f271c124a1ef04df7eabb773
Ravart <me@ravart.cc>
2016-12-18 20:56:13 +0100

    Play!

--- Assets/Scripts/Damiano/Goal.cs
+++ Assets/Scripts/Damiano/Goal.cs
@@ -15,10 +15,12 @@ public class Goal : MonoBehaviour {
 		
 	}
 
-	void OnCollisonEnter(Collision c){
+	void OnColliderEnter(Collision c){
 
 		GameObject obj = c.transform.root.gameObject;
 		Player player = obj.GetComponent<Player> ();
+
+		Debug.Log (c.gameObject.name);
 
 		if (player != null && player.faction != faction) {
 			Debug.Log (obj.name);
