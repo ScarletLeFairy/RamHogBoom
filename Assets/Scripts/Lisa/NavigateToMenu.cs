527f090aa7ab836a74ff6ba574acdffae57fe9d8
Lisa Gonaus <lisa.gonaus@students.fh-hagenberg.at>
2016-12-18 13:32:52 +0100

    Changed the Pause Menu, added some sounds

--- Assets/Scripts/Lisa/NavigateToMenu.cs
+++ Assets/Scripts/Lisa/NavigateToMenu.cs
@@ -5,27 +5,38 @@ public class NavigateToMenu : MonoBehaviour
 {
     public GameObject MenuToHide, MenuToShow;
 	public GameObject Background;
+	static GameObject PreviousMenu;
 
     public void SwitchMenu()
     {
         MenuToHide.SetActive(false);
-        MenuToShow.SetActive(true);
+		MenuToShow.SetActive(true);
+		PreviousMenu = MenuToHide;
     }
+
+	public void NavigateBack(){
+		MenuToHide.SetActive (false);
+		PreviousMenu.SetActive (true);
+		PreviousMenu = MenuToHide;
+	}
 
 	public void ShowMenu(GameObject menu){
 		menu.SetActive (true);
 	}
-	public void HideMenu(GameObject menu){
-		menu.SetActive (false);
-	}
 
-	public void HideMenuFully(GameObject menu){
-		Background.SetActive (false);
+	public void HideMenu(GameObject menu){
+		PreviousMenu = menu;
 		menu.SetActive (false);
 	}
 
 	public void ShowMenuFully(GameObject menu){
 		Background.SetActive (true);
 		menu.SetActive (true);
+	}
+
+	public void HideMenuFully(GameObject menu){
+		PreviousMenu = menu;
+		Background.SetActive (false);
+		menu.SetActive (false);
 	}
 }
