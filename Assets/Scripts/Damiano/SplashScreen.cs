4a74ffa14261956a604bbf9cb800d5317f749042
Ravart <me@ravart.cc>
2016-12-22 20:23:56 +0100

    SplashScreen

---
+++ Assets/Scripts/Damiano/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	float time;
+
+	// Use this for initialization
+	void Awake () {
+		time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (time + 3 <= Time.time) {
+			SceneManager.LoadScene("Menu");
+		}
+	}
+}
